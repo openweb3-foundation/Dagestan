482a46e92a5cc5ba9de6375919cb363dc84cc8d9
Muhammad-Jibril B.A <jbashir52@gmail.com>
2022-10-16 13:18:40 +0800

    Add `types` - Utils for STANCE

---
+++ types/src/lib.rs
@@ -0,0 +1,36 @@
+//! Traits that need to be implemented by the user.
+
+mod dataio;
+mod network;
+mod tasks;
+
+pub use stance_crypto::{
+    IncompleteMultisignatureError, Index, Indexed, Keychain, MultiKeychain, Multisigned, NodeCount,
+    NodeIndex, NodeMap, NodeSubset, PartialMultisignature, PartiallyMultisigned, Signable,
+    Signature, SignatureError, SignatureSet, Signed, UncheckedSigned,
+};
+pub use dataio::{DataProvider, FinalizationHandler};
+pub use network::{Network, Recipient};
+pub use tasks::{SpawnHandle, TaskHandle};
+
+use codec::Codec;
+use std::{fmt::Debug, hash::Hash as StdHash};
+
+/// Data type that we want to order.
+pub trait Data: Eq + Clone + Send + Sync + Debug + StdHash + Codec + 'static {}
+
+impl<T> Data for T where T: Eq + Clone + Send + Sync + Debug + StdHash + Codec + 'static {}
+
+/// A hasher, used for creating identifiers for blocks or units.
+pub trait Hasher: Eq + Clone + Send + Sync + Debug + 'static {
+    /// A hash, as an identifier for a block or unit.
+    type Hash: AsRef<[u8]> + Eq + Ord + Copy + Clone + Send + Sync + Debug + StdHash + Codec;
+
+    fn hash(s: &[u8]) -> Self::Hash;
+}
+
+/// The number of a session for which the consensus is run.
+pub type SessionId = u64;
+
+/// An asynchronous round of the protocol.
+pub type Round = u16;
